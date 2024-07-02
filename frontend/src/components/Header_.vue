133363baa5a24f1c420b080cba5ec00c47557897
Felipe Fugimoto <30874289+felipefugimoto@users.noreply.github.com>
2024-07-01 22:01:30 -0300

    Update Header_.vue

--- frontend/src/components/Header_.vue
+++ frontend/src/components/Header_.vue
@@ -2,10 +2,11 @@
     <header id="header" class="header fixed-top d-flex align-items-center">
 
         <div class="d-flex align-items-center justify-content-between">
-          <a href="index.html" class="logo d-flex align-items-center">
+          <router-link  class="logo d-flex align-items-center" to="/">
             <img src="@/assets/img/logo.png" alt="">
             <span class="d-none d-lg-block">NiceAdmin</span>
-          </a>
+        </router-link>
+         
          
         </div><!-- End Logo -->
     
